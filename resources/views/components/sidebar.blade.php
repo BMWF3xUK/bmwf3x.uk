3b267046b347eaba02eedc6310bd6ec25259ab12
Alan Wynn <alan.wynn@me.com>
2017-09-03 17:25:39 +0100

    change sidebar nav if active

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,9 +1,9 @@
 <ul class="list-group">
-    <a href="{{ route("home") }}" class="list-group-item active">
+    <a href="{{ route("home") }}" class="list-group-item {{ request()->route()->getName() == "home" ? "active" : null }}">
         Dashboard
     </a>
 
-    <a href="{{ route("guides.index") }}" class="list-group-item">
+    <a href="{{ route("guides.index") }}" class="list-group-item {{ starts_with(request()->route()->getName(), "guides.") ? "active" : null }}">
         Guides
     </a>
 </ul>
